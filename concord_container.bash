5818f2b0c7c19aa3c35180ae7423c6d4565b233f
Cole Brown <bigswim@gmail.com>
2015-06-18 12:49:34 -0400

    Port start script to use and configure b2d

--- concord_container.bash
+++ concord_container.bash
@@ -1,8 +1,8 @@
 #!/bin/bash --login
 
-DOCKERMACHINE=0
+BOOT2DOCKER=0
 NPROCS=1
-MEM=2048
+MEM=4096
 
 if [ $OSTYPE == "linux-gnu" ]; then
   NPROCS=$(grep -c ^processor /proc/cpuinfo)
@@ -18,27 +18,39 @@ USABLE_PROCS=`expr $NPROCS / 2`
 # 3/4 memory
 USABLE_MEM=`expr $MEM / 1024 / 4 \* 3`
 
-if [ $(command -v docker-machine) ]; then
-  DOCKERMACHINE=1
-  if [ $(docker-machine ls | grep -c concord-dev) == 0 ]; then
-    echo "Did not find Docker machine concord-dev. Creating machine with $USABLE_MEM mb memory..."
-    docker-machine create -d virtualbox --virtualbox-memory "$USABLE_MEM" --virtualbox-boot2docker-url "https://github.com/SvenDowideit/boot2docker/releases/download/x64rc1/boot2docker.iso" concord-dev
-  else
-    if [ $(docker-machine ls | grep -cP 'concord-dev.+Running') == 0 ]; then
-      echo "Docker machine concord-dev not running. Starting..."
-      docker-machine start concord-dev
+if [ $(command -v boot2docker) ]; then
+  BOOT2DOCKER=1
+
+  # check to see if there is a box (status code 0 == box exists)
+  boot2docker status > /dev/null 2>&1
+
+  if [ $? -ne 1 ]; then
+    # if there is a box already, ensure it's configured
+    boot2docker_memory=$(boot2docker config | grep Memory | awk '{print $3}')
+
+    if [ $(expr $boot2docker_memory \< $USABLE_MEM) -ne 0 ]; then
+      # if memory is too low, delete and reinit
+      boot2docker delete
+      boot2docker init -m $USABLE_MEM
     fi
+  else
+    # if there isn't a box, init it
+    boot2docker init -m $USABLE_MEM
   fi
-  echo "Ensuring machine environment is set..."
-  eval "$(docker-machine env concord-dev)"
+
+  # start the box (idempotent)
+  boot2docker start
+  # set environment
+  eval `boot2docker shellinit`
 fi
 
 CMD="docker run -t -i -v $(pwd):/workspace -p 5050:5050 -p 5051:5051 -p 9000:9000"
 
-if [ $DOCKERMACHINE -ne 0 ]; then
-  DOCKER_IP=$(docker-machine ip concord-dev)
-  CMD="$CMD -e \"VBOX_HOST=$DOCKER_IP\""
+if [ $BOOT2DOCKER -ne 0 ]; then
+  # boot2docker systems need to share vbox ip to container
+  CMD="$CMD -e \"VBOX_HOST=$(boot2docker ip)\""
 else
+  # linux systems need sudo
   CMD="sudo $CMD"
 fi
 
@@ -46,8 +58,5 @@ CMD="$CMD concord/client_devbox"
 
 echo "Running $CMD"
 
-# https://github.com/docker/machine/issues/641
-# last two comments
-
-# exec $CMD
+exec $CMD
 
