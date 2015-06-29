c771e56bc3374042a246edf26c8e6e555f51d8a1
Cole Brown <bigswim@gmail.com>
2015-06-29 18:52:02 -0400

    Add explanatory comments

--- rb/word_counter.rb
+++ rb/word_counter.rb
@@ -13,10 +13,16 @@ class WordCounter
     self.total_words = 0
   end
 
+  # init takes on argument:
+  # - context: the context object provided by the framework, used to communicate
+  #   with the framework
   def init(context)
     log "Initialized word counter"
   end
 
+  # process_record takes two arguments:
+  # - context: the context object used to interact with the framework
+  # - record: the incoming record to be processed
   def process_record(context, record)
     key = record.key
     self.words[key] ||= 0
@@ -26,6 +32,8 @@ class WordCounter
     end
   end
 
+  # metadata takes no arguments, but expects a return value of a `Metadata`
+  # object. check out the ruby client api documentation for more information.
   def metadata
     log "Metadata called"
     Concord::Metadata.new(name: 'word-counter',
--- rb/word_generator.rb
+++ rb/word_generator.rb
@@ -7,18 +7,23 @@ def log(str)
   $stderr.flush
 end
 
-
 def time_millis()
   (Time.now.to_f * 1000.0).to_i
 end
 
 class WordGenerator
+  # init takes one parameter:
+  # - context: the context object used to interact with the framework
   def init(context)
     future = time_millis
     log "Initialized Computation"
     context.set_timer('default', time_millis)
   end
 
+  # process_timer takes three arguments:
+  # - context: the context object used to interact with the framework
+  # - key: the name of the timer that's being triggered
+  # - time: the time the timer was scheduled to trigger at
   def process_timer(context, key, time)
     (0..1024).each do |i|
       context.produce_record('words', DICTIONARY.sample, '')
@@ -26,6 +31,8 @@ class WordGenerator
     context.set_timer(key, time_millis + 5000)
   end
 
+  # metadata takes no arguments, but expects a return value of a `Metadata`
+  # object. check out the ruby client api documentation for more information.
   def metadata
     log "Metadata called"
     Concord::Metadata.new(name: 'word-generator', ostreams: ['words'])
