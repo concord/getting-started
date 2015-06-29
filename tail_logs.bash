4c2404dbc25c9d06122bf94ae1378525051da09d
agallego <gallego.alexx@gmail.com>
2015-06-29 17:27:15 -0400

    Add script to tail logs inside a docker container

---
+++ tail_logs.bash
@@ -0,0 +1,13 @@
+#/bin/bash --login
+set -x
+
+TASK_DIRECTORY=$(find /var/lib/mesos/slaves/ -iname "$1")/
+
+if [[ ! -d ${TASK_DIRECTORY} ]]; then
+    echo "Task directory in /var/lib/mesos/slaves not found"
+    exit 1
+fi
+
+echo "Tailing stdout & err"
+
+tail -f ${TASK_DIRECTORY}/../std*
