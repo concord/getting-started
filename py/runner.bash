01e3d65b5c891e9f9fc377ce9925cb74d89adbca
agallego <gallego.alexx@gmail.com>
2015-07-30 18:36:15 -0400

    Simplify getting started

--- py/runner.bash
+++ py/runner.bash
@@ -1,6 +1,3 @@
 #!/bin/bash --login
-virtualenv .env
-source .env/bin/activate
-pip install -r requirements.txt
 echo "Directory: $(pwd)"
 exec python "$@"
--- rb/runner.bash
+++ rb/runner.bash
@@ -1,10 +1,4 @@
 #!/bin/bash --login
 
-set -ex
-
 echo "Directory: $(pwd)"
-
-
-gem2.0 install bundler
-bundle install
 exec bundle exec ruby2.0 "$@"
