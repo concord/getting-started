f06008eebdb22081d2d9853c7c650f3c0d9a4b12
Rob Blafford <rob@concord.io>
2016-01-11 16:22:43 -0500

    Converting python example to 3 computation wc

---
+++ py/sentence_splitter.py
@@ -0,0 +1,25 @@
+from concord.computation import (
+    Computation,
+    Metadata,
+    serve_computation
+)
+from concord.internal.thrift.ttypes import StreamGrouping
+
+class SentenceSplitter(Computation):
+    def init(self, ctx):
+        self.concord_logger.info("Splitter initialized")
+
+    def process_timer(self, ctx, key, time):
+        raise Exception('process_timer not implemented')
+
+    def process_record(self, ctx, record):
+        for word in record.key.split(" "):
+            ctx.produce_record('words', word, '-')
+
+    def metadata(self):
+        return Metadata(
+            name='sentence-splitter'
+            istreams=[('sentences', StreamGrouping.GROUP_BY)]
+            ostreams='words')
+
+serve_computation(SentenceSplitter())
